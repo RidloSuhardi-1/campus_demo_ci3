7b3192e6dbfe22e74d1ca2c609de9f993175c2d9
RidloSuhardi-1 <ridlosuhardi12@gmail.com>
2022-01-11 01:34:28 +0700

    Ubah catatan versi aplikasi

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -16,7 +16,7 @@
         </ul>
 
         <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-            <span>Ridlo Suhardi - CI 4</span>
+            <span>Ridlo Suhardi - CI 3</span>
             <a class="link-secondary" href="#" aria-label="Add a new report">
                 <span data-feather="alert-circle"></span>
             </a>
